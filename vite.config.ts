b72bcf287595621c698d9ce3da0b2a29b6d47e56
gtut_Betty <gtut3252300@gmail.com>
2025-11-25 21:38:23 +0800

    更新

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'site-map',
   plugins: [vue(), basicSsl()],
   resolve: {
     alias: {
